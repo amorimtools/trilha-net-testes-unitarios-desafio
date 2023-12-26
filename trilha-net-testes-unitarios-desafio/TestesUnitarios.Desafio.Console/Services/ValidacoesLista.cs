bd6b222ca880d1a844a5852d858b0665ddcbae0b
João Vitor <78668921+amorimtools@users.noreply.github.com>
2023-12-26 09:51:53 -0300

    Add files via upload

---
+++ trilha-net-testes-unitarios-desafio/TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestesUnitarios.Desafio.Console.Services
+{
+    public class ValidacoesLista
+    {
+        public List<int> RemoverNumerosNegativos(List<int> lista)
+        {
+            var listaSemNegativos = lista.Where(x => x > 0);
+            return listaSemNegativos.ToList();
+        }
+
+        public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
+        {
+            var contem = lista.Contains(numero);
+            return contem;
+        }
+
+        public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
+        {
+            var listaMultiplicada = lista.Select(x => x * numero).ToList();
+            return listaMultiplicada;
+        }
+
+        public int RetornarMaiorNumeroLista(List<int> lista)
+        {
+            return lista.Max();
+        }
+
+        public int RetornarMenorNumeroLista(List<int> lista)
+        {
+            return lista.Min();
+        }
+
+        public List<int> MultiplicarPorDois(List<int> lista)
+        {
+            var resultado = lista.Select(x => x * 2).ToList();
+            return resultado;
+        }
+    }
+}
